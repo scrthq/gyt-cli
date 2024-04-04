fcb672cb80c6d5aca8ae8a3d1e4c2c956e476acd
Nate Ferrell <nate@scrthq.com>
2024-04-04 14:54:45 -0500

    feat: #comment pytest updates

--- tests/test_gyt_cli.py
+++ tests/test_gyt_cli.py
@@ -1,12 +1,12 @@
 from typer.testing import CliRunner
 
 from gyt_cli.cli import app
+from gyt_cli import __version__
 
 runner = CliRunner()
 
 
 def test_app():
-    result = runner.invoke(app, ["Camila", "--city", "Berlin"])
+    result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
-    assert "Hello Camila" in result.stdout
-    assert "Let's have a coffee in Berlin" in result.stdout
+    assert __version__ in result.stdout
