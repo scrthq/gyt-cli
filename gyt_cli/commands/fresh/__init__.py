8c3d0f24a74ab3bce5deccf72798227ca11c1833
Nate Ferrell <nate@scrthq.com>
2024-04-03 21:11:44 -0500

    feat: #comment added fresh

--- gyt_cli/commands/fresh/__init__.py
+++ gyt_cli/commands/fresh/__init__.py
@@ -1,12 +1,25 @@
-# import subprocess
-# import git
-# import re
-# from typing_extensions import Annotated
+import subprocess
+import git
+import re
 import typer
+from typing_extensions import Annotated
 
 app = typer.Typer(
     help="Starts a fresh feature branch and syncs with the remote.",
 )
+@app.callback(invoke_without_command=True, no_args_is_help=True)
+def fresh(
+    branch:  Annotated[
+        str,
+        typer.Argument(
+            help="Name of the feature branch to be created.",
+        )
+    ],
+):
+    repo = git.Repo(search_parent_directories=True)
+    rgit: git.Git = repo.git
+    rgit.checkout('HEAD', b=branch)
+    rgit.push('origin', '-u', branch)
 
 if __name__ == "__main__":
     app()
